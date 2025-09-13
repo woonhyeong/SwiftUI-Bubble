dc737196e77bde0321be1a10474563cc00398399
Johnny <tlsehddkql12@gmail.com>
2025-09-14 01:18:54 +0900

    fix. change BubbleConfiguration' access

--- Sources/SwiftUIBubble/Configuration/BubbleConfiguration.swift
+++ Sources/SwiftUIBubble/Configuration/BubbleConfiguration.swift
@@ -8,30 +8,30 @@
 import SwiftUI
 
 public struct BubbleConfiguration {
-    var arrowAlignment: BubbleArrowAlignment = .bottom
-    var arrowHeight: CGFloat = 5
-    var innerPadding: EdgeInsets = .init(top: 5, leading: 5, bottom: 5, trailing: 5)
-    var border: BubbleBorder = BubbleBorder()
+    public var arrowAlignment: BubbleArrowAlignment = .bottom
+    public var arrowHeight: CGFloat = 5
+    public var innerPadding: EdgeInsets = .init(top: 5, leading: 5, bottom: 5, trailing: 5)
+    public var border: BubbleBorder = BubbleBorder()
     
-    func arrowAlignment(_ alignment: BubbleArrowAlignment) -> BubbleConfiguration {
+    public func arrowAlignment(_ alignment: BubbleArrowAlignment) -> BubbleConfiguration {
         var config = self
         config.arrowAlignment = alignment
         return config
     }
     
-    func arrowHeight(_ height: CGFloat) -> BubbleConfiguration {
+    public func arrowHeight(_ height: CGFloat) -> BubbleConfiguration {
         var config = self
         config.arrowHeight = height
         return config
     }
     
-    func innerPadding(_ insets: EdgeInsets) -> BubbleConfiguration {
+    public func innerPadding(_ insets: EdgeInsets) -> BubbleConfiguration {
         var config = self
         config.innerPadding = insets
         return config
     }
     
-    func border(_ border: BubbleBorder) -> BubbleConfiguration {
+    public func border(_ border: BubbleBorder) -> BubbleConfiguration {
         var config = self
         config.border = border
         return config
