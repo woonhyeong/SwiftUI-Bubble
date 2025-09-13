25ce5ee1c06805d0f1cb745f5cb194999bea2dfa
Johnny <tlsehddkql12@gmail.com>
2025-09-14 01:14:59 +0900

    feat. rename the target

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftUIBubble",
-            targets: ["Bubble"]),
+            targets: ["SwiftUIBubble"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Bubble"
+            name: "SwiftUIBubble"
         ),
     ]
 )
